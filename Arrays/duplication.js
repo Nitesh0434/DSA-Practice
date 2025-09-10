5225d5e461c66247ce170d770dae1eebae389c2d
Nitesh Kamat <nitesh0434@.com>
2025-09-10 21:42:51 +0545

    mistake solved

--- Arrays/duplication.js
+++ Arrays/duplication.js
@@ -12,22 +12,7 @@
  * 
  * Time Complexity: O(n)  // We scan each element once.
  * Space Complexity: O(n) // In worst case, store all elements in the Set.
- *//**
- * Problem: Contains Duplicate
- * Link: https://leetcode.com/problems/contains-duplicate/
- * Difficulty: Easy
- * 
- * Approach:
- * - Use a HashSet (Set in JS) to keep track of seen numbers.
- * - Traverse the array:
- *    - If the number already exists in the set → return true (duplicate found).
- *    - Else, add it to the set.
- * - If no duplicates are found after traversal → return false.
- * 
- * Time Complexity: O(n)  // We scan each element once.
- * Space Complexity: O(n) // In worst case, store all elements in the Set.
- */
-
+**/ 
 class Solution {
     /**
      * @param {number[]} nums
